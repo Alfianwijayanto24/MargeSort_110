5706b710200b5add11aa1e8e7755a16cba3ceace
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2026-03-31 11:21:02 +0700

    Membatasi jumlah input maksimal 20 elemen

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int arr[20],  B[20];
 int n;
 /// nama repo : MargeSort_110
-
+// Commit 2: Struktur dasar program
 void input(){
     while (true)
     {
@@ -24,79 +24,4 @@ void input(){
     cout << "\n======================================"<< endl;
     cout << "\n=======================Masukan Array==========="<< endl;
     cout << "\n======================================"<< endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Data ke-" << (i + 1) << ":";
-        cin >> arr[i];
-    }
-}
-
-void mergeSort(int low, int high){
-    if(low >= high)
-    {
-        return;
-    }
-    int mid = (low + high) / 2;
-
-
-    mergeSort(low, mid);
-    mergeSort( mid +  1, high);
-
-    int i = low;
-    int j = mid + 1;
-    int k = low;
-
-    while(i <= mid && j <= high)
-    {
-        if (arr[i] <= arr[j]<= high)
-        {
-            B[k] = arr[i];
-            i++;
-
-        }
-        else
-        {
-            B[k] = arr[j];
-            j++;
-        }    
-        k++;
-    }
-    while (j <= high)
-    {
-        B[k] = arr[j];
-        j++;
-        k = k + 1;
-
-    }
-    while (i <= mid)
-    
-    {
-        B[k] = arr[i];
-        i++;
-        k++;
-    
-    }
-    for (int x = low; x <= high; x++)
-    {
-        arr[x] = B[x];
-
-    }
-
-}
-void output()
-{
-    cout << "\nData setelah diurutakan (Merge Sort): ";
-    for(int i = 0; i <n; i++)
-    {
-        cout << arr[i]<< " ";
-
-    }
-    cout << endl;
-}
-int main()
-{
-    input();
-    mergeSort(0, n - 1);
-    output();
-
 }
